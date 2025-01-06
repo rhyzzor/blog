40665aa4e718d03ff9d6901c0907c32f47b8564e
Ryan Vieira Souza <ryanvsouza.dev@gmail.com>
2025-01-05 22:54:23 -0300

    feat: implements a Header with Nav and title

---
+++ components/AppHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+
+    <NuxtLink to="/" class="title">
+      <h1>Rhyzzor's Blog</h1>
+    </NuxtLink>
+
+    <AppNav />
+  </header>
+
+</template>
+
+
+<style>
+.title {
+  text-decoration: none;
+  color: inherit;
+
+  :hover {
+    text-decoration: underline;
+  }
+}
+
+.title h1 {
+  font-size: 1.75rem;
+}
+
+header {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  justify-content: space-between;
+}
+</style>
