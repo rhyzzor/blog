502d2af98a99357557a67f68b63c9246235604e2
Ryan Vieira Souza <ryanvsouza.dev@gmail.com>
2025-01-06 09:39:37 -0300

    feat: implements a post component

---
+++ components/Post.vue
@@ -0,0 +1,31 @@
+<template>
+  <NuxtLink :to="`/posts/${slug}`">
+    <strong><p>{{  date }} - {{ title }}</p> </strong>
+    <p>{{ description }}</p>
+  </NuxtLink>
+</template>
+
+<script lang="ts" setup>
+  const props = defineProps({
+    title: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    tags: {
+      type: Array,
+    },
+    date: {
+      type: String,
+    }
+  })
+
+  const slug = slugify(props?.title)
+
+
+</script>
+
+<style>
+
+</style>
