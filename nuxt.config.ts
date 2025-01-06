b21d7524ef45fe359c96bd77b2a4b4bfc04e8ae1
Ryan Vieira Souza <ryanvsouza.dev@gmail.com>
2025-01-06 09:40:32 -0300

    lint: change nuxt config formatter and linter

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  modules: [
-    '@nuxt/content',
-  ],
-  routeRules: {
-    '/': { prerender: true }
-  },
-  content: {
-    documentDriven: true,
-  },
-  app: {
-    head: {
-      title: "Rhyzzor's Blog",
-      meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      ],
-    },
-  },
-})
+	compatibilityDate: "2024-11-01",
+	devtools: { enabled: true },
+	modules: ["@nuxt/content"],
+	routeRules: {
+		"/": { prerender: true },
+	},
+	content: {
+		documentDriven: true,
+	},
+	app: {
+		head: {
+			title: "Rhyzzor's Blog",
+			meta: [
+				{ charset: "utf-8" },
+				{ name: "viewport", content: "width=device-width, initial-scale=1" },
+			],
+		},
+	},
+});
