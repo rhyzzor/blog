e1d8f9f781710378cfa7a66e973e84b7fcaee884
Ryan Vieira Souza <ryanvsouza.dev@gmail.com>
2025-01-06 09:40:04 -0300

    feat: implements a page to get all posts

---
+++ pages/posts/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <h2>Posts</h2>
+      <div v-for="post in data" :key="post._path">
+        <Post v-bind="post" />
+      </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+  const { data } = await useAsyncData('posts', () => queryContent('posts').find())
+
+</script>
+
+<style>
+
+</style>
