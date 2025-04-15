25175dd290d88e17a3fba42e614ceff0f888db19
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-04-15 15:37:18 -0300

    chore: add biome

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/fonts', '@nuxt/image']
-})
+	compatibilityDate: "2024-11-01",
+	devtools: { enabled: true },
+	modules: ["@nuxt/content", "@nuxt/fonts", "@nuxt/image"],
+});
