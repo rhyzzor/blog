499e019850bcdde30f32ca987a5a6580c6fc8e6f
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-04-15 18:18:53 -0300

    feat: add jet brains mono preload

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,9 @@ export default defineNuxtConfig({
 	compatibilityDate: "2024-11-01",
 	devtools: { enabled: true },
   fonts: {
-    provider: 'google',
+    families: [
+      { name: 'JetBrains Mono', weight: 300, provider: 'google', global: true, preload: true  },
+    ],
   },
   css: ["@/assets/css/main.css"],
 	modules: ["@nuxt/content", "@nuxt/fonts", "@nuxt/image"],
