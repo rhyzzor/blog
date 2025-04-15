bab5ac5f7034e41b11af01851cb3009e0feb97ab
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-04-15 18:02:57 -0300

    feat: set nuxt layout and nuxt page component

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="description">
+    <p>I'm a software developer from Brazil, currently 22 years old. This blog serves as a personal journal where I
+      share my interests and document my journey - both in my career and life. It's a space to reflect on my experiences
+      and keep track of everything I've learned and accomplished along the way.
+    </p>
+
+    <p>
+      This is the <a href="https://github.com/rhyzzor/blog" target="_blank">blog repository</a>, you can see the
+      source code of this blog.
+    </p>
+  </section>
+
+  <AppListPosts />
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+.description {
+  display: flex;
+  flex-direction: column;
+  gap: var(--spacing);
+}
+</style>
