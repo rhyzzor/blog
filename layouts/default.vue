a92d94639a0e51b6e5d646343f76de9e4c6a2075
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-04-15 16:01:01 -0300

    feat: add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <AppHeader />
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+
+</script>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  gap: var(--spacing);
+  position: relative;
+}
+</style>
