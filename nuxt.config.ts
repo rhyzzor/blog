0a0a2a156127732f112ea9121968b27ec2b23577
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-04-15 18:02:31 -0300

    chore: set font provider

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
 	compatibilityDate: "2024-11-01",
 	devtools: { enabled: true },
+  fonts: {
+    provider: 'google',
+  },
   css: ["@/assets/css/main.css"],
 	modules: ["@nuxt/content", "@nuxt/fonts", "@nuxt/image"],
 });
