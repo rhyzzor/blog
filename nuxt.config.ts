dd772e89e025e79857cb9ee71c3bdc88b1b8ccf2
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-04-15 20:20:21 -0300

    feat:  add content options

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,19 @@
 export default defineNuxtConfig({
 	compatibilityDate: "2024-11-01",
 	devtools: { enabled: true },
+  content: {
+    renderer: {
+      anchorLinks: false,
+    },
+    build: {
+      markdown: {
+        highlight: {
+          langs: ['js', 'ts', 'go', 'python', 'bash', 'cpp', "json"],
+          theme: "nord"
+        }
+      }
+    }
+  },
   fonts: {
     families: [
       { name: 'JetBrains Mono', weight: 300, provider: 'google', global: true, preload: true  },
