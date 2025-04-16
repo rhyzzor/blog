b8ba7c15578bb8c9c8437261fc915f6acf36b59c
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-04-15 22:48:38 -0300

    feat: add option to add hour

--- utils/date-format.ts
+++ utils/date-format.ts
@@ -1,5 +1,24 @@
-export default (date: Date) => {
- const newDate = new Date(date).toISOString().split('T')[0];
 
- return newDate;
-}
+const formatDate = (date: Date | string | number, withTime = false): string => {
+  const validDate = date instanceof Date ? date : new Date(date);
+  
+  if (isNaN(validDate.getTime())) {
+    throw new Error('Invalid date provided');
+  }
+
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    ...(withTime && {
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false,
+    }),
+  };
+
+  return new Intl.DateTimeFormat('pt-BR', options).format(validDate);
+};
+
+export default formatDate;
