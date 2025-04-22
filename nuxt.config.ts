870c4afc0d540f13392fbfedcc9250ab4d8db262
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-04-21 22:23:58 -0300

    chore: add transition

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ import { definePerson } from "nuxt-schema-org/schema";
 export default defineNuxtConfig({
 	compatibilityDate: "2024-11-01",
 	devtools: { enabled: true },
+  app: {
+    pageTransition: { name: "slide-right", mode: "out-in" },
+  },
 	robots: {
 		allow: "*",
 	},
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,22 @@
 <template>
-  <section class="description">
-    <p>I'm a software developer from Brazil, currently 22 years old. This blog serves as a personal journal where I
-      share my interests and document my journey - both in my career and life. It's a space to reflect on my experiences
-      and keep track of everything I've learned and accomplished along the way.
-    </p>
+  <div>
+    <section class="description">
+      <p>I'm a software developer from Brazil, currently 22 years old. This blog serves as a personal journal where I
+        share my interests and document my journey - both in my career and life. It's a space to reflect on my
+        experiences
+        and keep track of everything I've learned and accomplished along the way.
+      </p>
 
-    <p>
-      This is the <a href="https://github.com/rhyzzor/blog" target="_blank">blog repository</a>, you can see the
-      source code of this blog.
-    </p>
-  </section>
+      <p>
+        This is the <a href="https://github.com/rhyzzor/blog" target="_blank">blog repository</a>, you can see the
+        source code of this blog.
+      </p>
+    </section>
 
-  <AppListPosts />
+
+    <AppListPosts />
+  </div>
+
 </template>
 
 <script lang="ts" setup>
