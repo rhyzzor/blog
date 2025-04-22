c0a6ec0ca5a91490ae6402179165b7a97ae92cce
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-04-21 23:59:05 -0300

    chore: delete unused class

--- app.vue
+++ app.vue
@@ -14,21 +14,9 @@ useHead({
 </script>
 
 <style>
-.slide-left-enter-active,
-.slide-left-leave-active,
 .slide-right-enter-active,
 .slide-right-leave-active {
   transition: all 0.2s;
-}
-
-.slide-left-enter-from {
-  opacity: 0;
-  transform: translate(50px, 0);
-}
-
-.slide-left-leave-to {
-  opacity: 0;
-  transform: translate(-50px, 0);
 }
 
 .slide-right-enter-from {
